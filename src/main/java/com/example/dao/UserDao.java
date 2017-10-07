2ff35f80ddf8ef284270e6027f09a3362d65b823
Shubham <escortnotice@gmail.com>
2017-10-08 00:17:46 +0530

    added @Transactional (rollbackFor= CustomDaoException.class) in service class

--- src/main/java/com/example/dao/UserDao.java
+++ src/main/java/com/example/dao/UserDao.java
@@ -11,14 +11,7 @@ import org.springframework.stereotype.Repository;
 import com.example.customexceptionhandling.CustomDaoException;
 import com.example.model.User;
 
-/**
- * The Custom Dao exception thrown should be a subclass of Unchecked exception, else the exception
- * will not be propagated and handled by Controller Advice Exception handler class(ExceptionHandlerAdvice.java).
- * 
- * This happens because only unchecked exceptions cause rollbacks in spring transactions.
- * 
- * Or you can use @Transactional (rollbackFor= CustomDaoException.class) if customdaoexception class is checked exception class.
- */
+
 @Repository
 public class UserDao {
 
@@ -29,6 +22,14 @@ public class UserDao {
 
 	public static final String OK = "OK";
 	
+	/**
+	 * The Custom Dao exception thrown should be a subclass of Unchecked exception, else the exception
+	 * will not be propagated and handled by Controller Advice Exception handler class(ExceptionHandlerAdvice.java).
+	 * 
+	 * This happens because only unchecked exceptions cause rollbacks in spring transactions.
+	 * 
+	 * Or you can use @Transactional (rollbackFor= CustomDaoException.class) if customdaoexception class is checked exception class.
+	 */
 	// creates a new user
 	public String createUser(User user) throws CustomDaoException {
 		Session session = null;
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -16,7 +16,15 @@ public class UserService {
 	@Autowired 
 	UserDao userDao;
 	
-	@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT)
+	/**
+	 * The Custom Dao exception thrown should be a subclass of Unchecked exception, else the exception
+	 * will not be propagated and handled by Controller Advice Exception handler class(ExceptionHandlerAdvice.java).
+	 * 
+	 * This happens because only unchecked exceptions cause rollbacks in spring transactions.
+	 * 
+	 * Or you can use @Transactional (rollbackFor= CustomDaoException.class) if customdaoexception class is checked exception class.
+	 */
+	@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT,rollbackFor=CustomDaoException.class)
 	public String createUser(User user) throws CustomDaoException{
 		return userDao.createUser(user);
 	}
